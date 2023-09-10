f671686568c8f98fa084fca69fcd36b041b90994
hitoshidevx <g.hitoshi.yokogawa@gmail.com>
2023-09-10 11:22:44 -0300

    Formulario adicionado com inputs adicionado e navegação implementada

--- smartsellers/App.js
+++ smartsellers/App.js
@@ -13,9 +13,11 @@ import {
 import { TextInput } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import axios from 'axios'
 import banner from './assets/bannerChatbot.jpg'
 import chatbotImage from './assets/chatbotCelular.png'
+
 
 const Stack = createStackNavigator();
 
@@ -71,49 +73,8 @@ const Home = ({ navigation }) => {
     }
 
   })
-
-  const Menu = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-
-  return (
-    <View style={{alignSelf: "center"}}>
-      <Pressable onPress={() => setModalVisible(true)} >
-      </Pressable>
-
-      <Modal
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(false);
-        }}
-      >
-      
-      <View style= {{backgroundColor: "#025A90", width: "50%", height: "25%", alignSelf: "flex-end"}}>
-
-        <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>  
-          <Pressable onPress={() => setModalVisible(false)}>
-          </Pressable>
-        </View>
-
-        <View>
-          <Text style = {estilos.textoMenu}>Sobre nós</Text>
-          <Text style = {estilos.textoMenu}>Cases</Text>
-          <Text style = {estilos.textoMenu}>Soluções</Text>
-          <Text style = {estilos.textoMenu}>Planos</Text>
-          <Text style = {estilos.textoMenu}>Contato</Text>
-        </View>
-
-        </View>
-      </Modal>
-    </View>
-  )
-}
   return (
     <View style = {{flex: 1}}>
-      <View style={{ flex: 1, backgroundColor: '#025A90', flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={estilos.nomeEmpresa}> SmartSellers </Text>
-        <Menu/>
-      </View>
 
       <View style={{ flex: 5 }}>
         <ImageBackground
@@ -131,11 +92,14 @@ const Home = ({ navigation }) => {
             </Text>
           </View>
 
-          <Pressable style={estilos.botao}>
-            <Text style={estilos.textoBotao}>
-              Saiba Mais!
+          <TouchableOpacity
+            style={estilos.botao}
+            onPress={() => navigation.navigate('Formulario')}>
+            <Text
+              style={estilos.textoBotao}>
+              Prosseguir
             </Text>
-          </Pressable>
+          </TouchableOpacity>
         </ImageBackground>
       </View>
 
@@ -157,6 +121,176 @@ const Home = ({ navigation }) => {
   );
 }
 
+const Inputting = ({ texto }) => {
+  const [pergunta, setPergunta] = useState("");
+
+
+  return (
+    <View style={{ flex: 1, marginBottom: 150 }}>
+      <View
+        style={{
+          backgroundColor: "#025A90",
+          padding: 20,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 20,
+            textAlign: "center",
+            fontWeight: 600,
+          }}
+        >
+          {texto}
+        </Text>
+      </View>
+      <TextInput
+        placeholder="Digite aqui..."
+        placeholderTextColor="white"
+        style={{
+          backgroundColor: "#04192C",
+          color: "white",
+          padding: 20,
+          textAlign: "center",
+          borderBottomRightRadius: 10,
+          borderBottomLeftRadius: 10,
+        }}
+        onChangeText={(text) => setPergunta(text)}
+        value={pergunta}
+      />
+    </View>
+  );
+};
+
+const Formulario = () => {
+
+  const [nomeProduto, setNomeProduto] = useState("")
+  const [descricaoProduto, setDescricaoProduto] = useState("")
+
+  return(
+    <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#00284D',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: '#00284D',
+              justifyContent: 'space-around',
+              width: "100%",
+              height: "100%",
+              padding: 50,
+              marginTop: 30
+            }}>
+
+            
+            <View style={{ flex: 1}}>
+                  <View
+                    style={{
+                      backgroundColor: "#025A90",
+                      padding: 20,
+                      borderTopLeftRadius: 10,
+                      borderTopRightRadius: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 20,
+                        textAlign: "center",
+                        fontWeight: 600,
+                      }}
+                    >
+                      Digite o nome do seu produto
+                    </Text>
+                  </View>
+                  <TextInput
+                    placeholder="Digite aqui..."
+                    placeholderTextColor="white"
+                    style={{
+                      backgroundColor: "#04192C",
+                      color: "white",
+                      padding: 20,
+                      textAlign: "center",
+                      borderBottomRightRadius: 10,
+                      borderBottomLeftRadius: 10,
+                    }}
+                    onChangeText={(text) => setNomeProduto(text)}
+                    value={nomeProduto}
+                  />
+                </View>
+
+                <View style={{ flex: 1 }}>
+                  <View
+                    style={{
+                      backgroundColor: "#025A90",
+                      padding: 20,
+                      borderTopLeftRadius: 10,
+                      borderTopRightRadius: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "white",
+                        fontSize: 20,
+                        textAlign: "center",
+                        fontWeight: 600,
+                      }}
+                    >
+                      Descreva o seu produto
+                    </Text>
+                  </View>
+                  <TextInput
+                    placeholder="Digite aqui..."
+                    placeholderTextColor="white"
+                    style={{
+                      backgroundColor: "#04192C",
+                      color: "white",
+                      padding: 20,
+                      textAlign: "center",
+                      borderBottomRightRadius: 10,
+                      borderBottomLeftRadius: 10,
+                    }}
+                    onChangeText={(text) => setDescricaoProduto(text)}
+                    value={descricaoProduto}
+                  />
+                </View>
+           
+
+
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#04192C',
+                width: '100%',
+                padding: 15,
+                alignSelf: 'center',
+                borderRadius: 10,
+                marginBottom: 50
+              }}
+              
+              >
+              <Text
+                style={{
+                  color: 'white',
+                  textAlign: 'center',
+                  fontWeight: 600,
+                  fontSize: 20,
+                }}>
+                Pronto!
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+    </View>
+  )
+}
+
 
 
 export default function App() {
@@ -166,7 +300,36 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{ headerShown: false }}
+          options={{
+            title: 'SmartSellers',
+            headerStyle: {
+              backgroundColor: '#025A90',
+              height: 150
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontSize: 30,
+              fontWeight: 700
+            },
+            headerTitleAlign: 'center'
+          }}
+        />
+        <Stack.Screen
+          name="Formulario"
+          component={Formulario}
+          options={{
+            title: 'Produto',
+            headerStyle: {
+              backgroundColor: '#025A90',
+              height: 150
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontSize: 30,
+              fontWeight: 700
+            },
+            headerTitleAlign: 'center'
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
